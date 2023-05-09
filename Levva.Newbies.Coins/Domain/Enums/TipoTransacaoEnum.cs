c04f275bb90a9eb22f530dfaebdc582aff16287b
Thaysa Zooby <thaysa.zooby@gmail.com>
2023-05-09 09:18:01 -0300

    feat: Implementacao das Models;

---
+++ Levva.Newbies.Coins/Domain/Enums/TipoTransacaoEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace Levva.Newbies.Coins.Domain.Enums
+{
+    public enum TipoTransacaoEnum
+    {
+        Entrada = 0,
+        Saida = 1,
+    }
+}
---
+++ Levva.Newbies.Coins/Domain/Models/Categoria.cs
@@ -0,0 +1,9 @@
+﻿namespace Levva.Newbies.Coins.Domain.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public virtual List<Transacao> Transacoes { get; set; }
+    }
+}
